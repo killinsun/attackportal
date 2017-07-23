84991b9cd3efc5aecb14181075622d417875ee5e
killinsun <noneck.0311@gmail.com>
2017-07-24 02:59:00 +0900

    Change dirctory tree.

---
+++ portal/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'portal'
+
+urlpatterns = [
+    url(r'^$', views.mainpage, name='mainpage'),
+    #url(r'^login/$', views.loginpage),
+    url(r'^redirect/$', views.redirect, name='redirect'),
+    url(r'^vmlist/$', views.vmlist, name='vmlist'),
+    url(r'^deploy/$', views.deploy, name='deploy'),
+    url(r'^complete_deploy/$', views.completeDeploy, name='complete_deploy'),
+    url(
+        r'^login/$', 
+        auth_views.login, 
+        { 'template_name': 'portal/login.html' },
+        name='login'
+    ),
+    url(
+        r'^logout/$', 
+        auth_views.logout, 
+        { 'template_name': 'portal/logout.html' },
+        name='logout'
+    ),
+]
